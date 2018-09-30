9dd5f175010ef610aebf2c7a46ff8fe30bbaf706
aravinthvp <34703046+aravinthvp@users.noreply.github.com>
2018-09-30 18:54:33 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,17 @@
+Deep learing pre requests
+
+#Numpy maths
+>>>import numpy as np
+>>>a=[1,2,3]
+>>>b=np.array(1,2,3)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ValueError: only 2 non-keyword arguments accepted
+>>> b=np.array([1,2,3])
+>>> np.log(b)
+array([ 0.        ,  0.69314718,  1.09861229])
+>>> np.exp(b)
+array([  2.71828183,   7.3890561 ,  20.08553692])
+>>> np.sqrt(b)
+array([ 1.        ,  1.41421356,  1.73205081])
+>>> 
